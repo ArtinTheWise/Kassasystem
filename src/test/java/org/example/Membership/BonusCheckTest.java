2c070514d71ff57b4e9db5f82a57ba525e297629
Ziper004 <leoziper04@gmail.com>
2025-10-20 17:41:51 +0200

    Skapat BonusCheckTest och ett test

--- src/test/java/org/example/Membership/BonusCheckTest.java
+++ src/test/java/org/example/Membership/BonusCheckTest.java
@@ -1,4 +1,26 @@
 package org.example.Membership;
 
-public class BonusCheckTest {
+import org.example.Discount.ProductDecorator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class BonusCheckTest {
+
+    @Test
+    void constructor_ShouldSetAllFieldsCorrectly() {
+
+        String expectedName = "10% Off Coffee";
+        ProductDecorator mockDiscount = mock(ProductDecorator.class);
+        Points expectedPoints = new Points(150);
+
+        BonusCheck bonusCheck = new BonusCheck(expectedName, mockDiscount, expectedPoints);
+
+        assertEquals(expectedName, bonusCheck.getName());
+        assertEquals(mockDiscount, bonusCheck.getDiscount());
+        assertEquals(expectedPoints, bonusCheck.getPointsToBuy());
+    }
+
 }
+
