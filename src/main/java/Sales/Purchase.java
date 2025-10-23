69304ae3f111bb0320888cd508bb361cfb236b99
Axelbackman <Axel.ulmgren@gmail.com>
2025-10-23 17:26:49 +0200

    Green addPieceWithWeightPricemodelThrowsException

--- src/main/java/Sales/Purchase.java
+++ src/main/java/Sales/Purchase.java
@@ -2,6 +2,8 @@ package Sales;
 
 
 import org.example.Product.Product;
+import org.example.Product.UnitPrice;
+import org.example.Product.UnitPriceWithPant;
 
 public class Purchase {
     public Purchase(Object cashRegister, Object salesEmployee){
@@ -17,6 +19,9 @@ public class Purchase {
     public void addPiece(Product product){
         if (product == null) {
             throw new IllegalArgumentException("Product cannot be null.");
+        }
+        if (!(product.getPriceModel() instanceof UnitPrice || product.getPriceModel() instanceof UnitPriceWithPant)){
+            throw new IllegalArgumentException("The product does not have piece pricemodel");
         }
     
 
