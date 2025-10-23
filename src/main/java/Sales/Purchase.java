42b03a4c26338cdb071b27e645b6603bcf785e7f
Axelbackman <Axel.ulmgren@gmail.com>
2025-10-23 17:34:41 +0200

    Green addWeight

--- src/main/java/Sales/Purchase.java
+++ src/main/java/Sales/Purchase.java
@@ -4,6 +4,7 @@ package Sales;
 import org.example.Product.Product;
 import org.example.Product.UnitPrice;
 import org.example.Product.UnitPriceWithPant;
+import org.example.Product.WeightPrice;
 
 public class Purchase {
     public Purchase(Object cashRegister, Object salesEmployee){
@@ -21,12 +22,18 @@ public class Purchase {
             throw new IllegalArgumentException("Product cannot be null.");
         }
         if (!(product.getPriceModel() instanceof UnitPrice || product.getPriceModel() instanceof UnitPriceWithPant)){
-            throw new IllegalArgumentException("The product does not have piece pricemodel");
+            throw new IllegalArgumentException("The product does not have PIECE price-model");
         }
     
     }
 
     public void addWeight(Product product){
+        if (product == null) {
+            throw new IllegalArgumentException("Product cannot be null.");
+        }
+        if (!(product.getPriceModel() instanceof WeightPrice)){
+            throw new IllegalArgumentException("The product does not have WEIGHT price-model");
+        }
         
     }
 
