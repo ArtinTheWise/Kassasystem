21a73af21758d267c0ef6948db82ef6dc0702e55
11-1-10100 <alexander.aimar.blanco@gmail.com>
2025-10-26 15:51:09 +0100

    CashRegister klass utan funktion

---
+++ src/main/java/Sales/CashRegister.java
@@ -0,0 +1,18 @@
+package Sales;
+
+public class CashRegister {
+
+    String cashRegisterID;
+
+
+    public CashRegister() {
+    }
+
+
+    public Purchase newPurchase(){
+        return null;
+    }
+
+
+
+}
