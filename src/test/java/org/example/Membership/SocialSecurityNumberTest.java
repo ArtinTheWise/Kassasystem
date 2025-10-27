ca5aba21f44b65a02b6382803748b73cfc3f1753
11-1-10100 <alexander.aimar.blanco@gmail.com>
2025-10-27 11:36:24 +0100

    början av SocialSecurityNumberTest

--- src/test/java/org/example/Membership/SocialSecurityNumberTest.java
+++ src/test/java/org/example/Membership/SocialSecurityNumberTest.java
@@ -2,6 +2,43 @@ package org.example.Membership;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
 class SocialSecurityNumberTest {
+
+    private final String validEmailAddress = "mail@example.com";
+
+    @Test
+    void T1_R2() {
+        Exception e = assertThrows(IllegalArgumentException.class, () -> {
+            Customer customerExample = new Customer("199012309999", validEmailAddress);
+        });
+        assertTrue(e.getMessage().contains("Invalid check digit"));
+    }
+
+    @Test
+    void T2_R2() {
+        Exception e = assertThrows(IllegalArgumentException.class, () -> {
+            Customer customerExample = new Customer("200004300000", validEmailAddress);
+        });
+        assertTrue(e.getMessage().contains("Invalid check digit"));
+    }
+
+    @Test
+    void T3_R5() {
+        assertDoesNotThrow(() -> {
+            Customer customerExample = new Customer("201002283343", validEmailAddress);
+        });
+    }
+
+    @Test
+    void T4_R5() {
+        assertDoesNotThrow(() -> {
+            Customer customerExample = new Customer("200402283341", validEmailAddress);
+        });
+    }
+
+
+
 
 }
