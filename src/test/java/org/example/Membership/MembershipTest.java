53ae87d611159142bfb807beb7e1bad784f37d20
11-1-10100 <alexander.aimar.blanco@gmail.com>
2025-10-28 11:22:01 +0100

    MembershipTest:en fixades, bad SSN

--- src/test/java/org/example/Membership/MembershipTest.java
+++ src/test/java/org/example/Membership/MembershipTest.java
@@ -13,15 +13,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MembershipTest {
 
+    private final String validSSN = "200001011234";
+    private final String validEmailAddress = "mail@example.com";
+
     @Test
     public void getCustomerTest() {
-        Customer customer = new Customer("200404301234", "test@example.com");
+        Customer customer = new Customer(validSSN, validEmailAddress);
         Membership membership = new Membership(customer);
         assertEquals(customer, membership.getCustomer());
     }
 
     @Test
-    public void getPointsTest() {}
+    public void getPointsTest() {} // todo finns igen sätt att skaffa points just nu
 
 
 
@@ -31,16 +34,16 @@ public class MembershipTest {
     //bonusCheckar
     @Test
     void getAddedChecksTest() {
-        Customer customer = new Customer("200404301234", "test@example.com");
+        Customer customer = new Customer(validSSN, validEmailAddress);
         Membership membership = new Membership(customer);
 
         membership.addCheck(new BonusCheck("snackSale",
                 new NormalDiscount(new Product("chips", new UnitPrice(new Money(30)),
-                        VatRate.FOOD, false),50, LocalDateTime.now().plusMonths(6)),
+                        VatRate.FOOD, false),20, LocalDateTime.now().plusMonths(6)),
                 new Points(200)));
         membership.addCheck(new BonusCheck("snackSale",
                 new NormalDiscount(new Product("chips", new UnitPrice(new Money(30)),
-                        VatRate.FOOD, false),50, LocalDateTime.now().plusMonths(6)),
+                        VatRate.FOOD, false),20, LocalDateTime.now().plusMonths(6)),
                 new Points(200)));                                                                   //dubblett ???
         membership.addCheck(new BonusCheck("santaSale",
                 new NormalDiscount(new Product("skumtomtar", new UnitPrice(new Money(50)),
